0155ca981041941dd59c237497a5d547b97406e2
Vipul Shah <30666580+vipgh0828@users.noreply.github.com>
2026-04-12 19:02:35 +0000

    Added requirements libray and import statements

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,11 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
+# New section to display smoothiefroot ntrition information
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
+
 # Write directly to the app.
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw: {st.__version__}")
 st.write(
