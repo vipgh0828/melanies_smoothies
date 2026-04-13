e4438b1de2f6cf578cf341afceb2d81a61e52fb5
Vipul Shah <30666580+vipgh0828@users.noreply.github.com>
2026-04-12 21:05:52 -0500

    Change display of smoothiefroot data to dataframe
    Updated the display method for smoothiefroot nutrition information.

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,5 @@ if ingredients_list:
 # New section to display smoothiefroot ntrition information
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
